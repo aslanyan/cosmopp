46c5cc63caa701850e27db423b4d38a60882a94b
aslanyan <g.aslanyan@gmail.com>
2014-06-19 16:53:01 +1200

    performance improvement to legendre

--- include/legendre.hpp
+++ include/legendre.hpp
@@ -11,23 +11,24 @@ namespace Math
 class Legendre
 {
 public:
-    Legendre() {}
+    Legendre() : vals_(10000) {}
     ~Legendre() {}
 
     double calculate(unsigned int l, double x)
     {
-        if(l == 0)
-            return 0.0;
-        if(l == 1)
-            return x;
+        if(vals_.size() < l + 1)
+            vals_.resize(l + 1);
 
-        std::vector<double> vals(l + 1, 1);
-        vals[1] = x;
+        vals_[0] = 1;
+        vals_[1] = x;
         for(int l1 = 2; l1 <= l; ++l1)
-            vals[l1] = (2 - 1.0 / l1) * x * vals[l1 - 1] - (1 - 1.0 / l1) * vals[l1 - 2];
+            vals_[l1] = (2 - 1.0 / l1) * x * vals_[l1 - 1] - (1 - 1.0 / l1) * vals_[l1 - 2];
 
-        return vals[l];
+        return vals_[l];
     }
+
+private:
+    std::vector<double> vals_;
 };
 
 } // namespace Math
