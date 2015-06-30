f5982e48f251fc18922b5f78f16ea09c2f168752
aslanyan <g.aslanyan@auckland.ac.nz>
2015-06-30 22:41:05 +1200

    small updates to pde interface

--- include/pde.hpp
+++ include/pde.hpp
@@ -17,8 +17,18 @@ public:
     virtual int spaceDim() const = 0;
     virtual int funcDim() const = 0;
 
-    virtual void f(const double *x, const double *u, double *res);
-    virtual void s(const double *x, const double *u, double *res);
+    virtual void f(double t, const double *x, const double *u, double *res);
+    virtual void s(double t, const double *x, const double *u, double *res);
+};
+
+class InitialValPDEOutputHandlerInterface
+{
+public:
+    InitialValPDEOutputHandlerInterface() {}
+    virtual ~InitialValPDEOutputHandlerInterface() {}
+
+    virtual void setGrid(const std::vector<double>& xMin, const std::vector<double>& xMax, const std::vector<int>& nx);
+    virtual void setTimeSlice(double t, double *res);
 };
 
 class InitalValPDESolver
@@ -27,11 +37,11 @@ public:
     InitialValPDESolver(InitialValPDEInterface *pde);
     ~InitialValPDESolver();
 
-    void set(const std::vector<FunctionMultiDim*>& w0, const std::vector<double>& xMin, const std::vector<double>& xMax, const std::vector<int>& nx, const char* outputRoot);
+    void set(const std::vector<FunctionMultiDim*>& w0, const std::vector<double>& xMin, const std::vector<double>& xMax, const std::vector<int>& nx, InitialValPDEOutputHandlerInterface* output);
 
     void propagate(double t);
 
-    double getCurrentT() const = 0;
+    double getCurrentT() const;
 };
 
 } // namespace Math
