05dfe1ad48d7c0a8ea0052dcc087be5e20b625ee
Grigor Aslanyan <aslanyan@berkeley.edu>
2016-05-16 16:30:49 -0700

    parser added

---
+++ source/test_parser.cpp
@@ -0,0 +1,33 @@
+#include <macros.hpp>
+#include <parser.hpp>
+
+int main(int argc, char *argv[])
+{
+    try {
+        StandardException exc;
+        if(argc < 2)
+        {
+            std::stringstream exceptionStr;
+            exceptionStr << "Need to pass at least one parameters file as an argument!";
+            exc.set(exceptionStr.str());
+            throw(exc);
+        }
+        
+        Parser p(argv[1]);
+        for(int i = 2; i < argc; ++i)
+            p.readFile(argv[i]);
+
+        p.dump();
+        double d = p.getDouble("d");
+        output_screen("d = " << d << std::endl);
+
+        int x= p.getInt("x", 15);
+        output_screen("x = " << x << std::endl);
+    } catch (std::exception& e)
+    {
+        output_screen("EXCEPTION CAUGHT!!! " << std::endl << e.what() << std::endl);
+        output_screen("Terminating!" << std::endl);
+        return 1;
+    }
+    return 0;
+}
